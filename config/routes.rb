abb40e1fb26c235db161fa3ffe673eadeac95363
hogucc <uku.h1r8@gmail.com>
2021-09-20 16:33:53 +0900

    gem letter_opener_webを導入し、パスワード変更メールをブラウザで確認できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: %i(index)
   resources :books, only: %i(index new create)
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
